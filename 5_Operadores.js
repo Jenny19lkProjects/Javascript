debdb4dd8aa551d83c63df3149549da50f04b1cf
Jenny19lkProyectos <jenny.19lk.projects@gmail.com>
2025-08-03 15:21:49 +0200

    Operadores

---
+++ 5_Operadores.js
@@ -0,0 +1,23 @@
+let num1=10;
+let num2=20;
+
+const sumResult=num1+num2;
+console.log("Suma:"+sumResult);
+
+const subResult=num1-num2;
+console.log("Resta:"+subResult);
+
+const multi=num1*num2;
+console.log(multi);
+
+const divideResult=num1/num2;
+console.log("Division: "+divideResult);
+
+num1=num1+5;
+console.log("num1 :"+num1);
+
+num1+=15;
+console.log("num1 :"+num1);
+
+const moduleResult=10%3;
+console.log("Module: " +moduleResult);
