628a22a6e92e894f19a848390f4293f177e2f273
Jenny19lkProyectos <jenny.19lk.projects@gmail.com>
2025-08-04 16:55:43 +0200

    Ternarios y truthy and falsy

---
+++ 10_Ternarios.js
@@ -0,0 +1,9 @@
+const condition=true;
+if(condition){
+    console.log("Se cumple");
+} else{
+    console.log("No se cumple");
+
+}
+const condition2=false;
+condition2 ? console.log("Ternario si") :console.log("Ternario no")
---
+++ b_11_Truthy && Falsy.js
@@ -0,0 +1,7 @@
+
+truthy:12334,"djkhsdjk",[],{}
+falsy:0, "", null, undefined
+if(true){
+    console.log("hola");
+    
+}
