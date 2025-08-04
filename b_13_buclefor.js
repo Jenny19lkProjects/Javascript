f3f5e0c84edf3fc82e9975e5357a0f10f31e210e
Jenny19lkProyectos <jenny.19lk.projects@gmail.com>
2025-08-04 18:45:41 +0200

    bucle for

---
+++ b_13_buclefor.js
@@ -0,0 +1,11 @@
+for (let i = 0; i < 10; i++) {
+  console.log("i: ", i);
+ }
+
+const users = ["Alberto", "Beatriz", "Carlos", "Daniel"];
+
+for (let i = 0; i < users.length; i++) {
+  console.log(users[i]);  
+}
+
+
