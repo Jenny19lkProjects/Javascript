c510d65b4445fc0d38661e08ec4526d8e2978183
Jenny19lkProyectos <jenny.19lk.projects@gmail.com>
2025-08-06 16:51:40 +0200

    Objeto avanzados

---
+++ c_21_Objetos_Avanzados.js
@@ -0,0 +1,28 @@
+/ Constructor Object()
+
+const movie = new Object();
+movie.title = "El Señor de los Anillos";
+movie.duration = 200;
+movie.year = 2001;
+
+console.log(movie);
+
+
+// Funciones constructoras
+
+function Person(firstName, lastName, age) {
+  this.firstName = firstName;
+  this.lastName = lastName;
+  this.age = age;
+}
+
+const user = new Person("Carlos", "López", 28);
+console.log(user);
+
+
+// Métodos de los objetos
+
+console.log(Object.keys(user));
+console.log(Object.values(user));
+console.log(Object.entries(user));
+console.log(user.hasOwnProperty("age"));
